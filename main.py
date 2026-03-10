01f99b29928a0e9731b0d544a02162581e0fe8eb
vanala23 <vanala23@htl-kaindorf.at>
2026-03-10 12:08:02 +0100

    feat: Add simple addition function

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import os
 import sys
 
+def add(a, b):
+    return a + b
+
 def main():
     print("Welcome to Math Magician!")
     op = sys.argv[1]
     a = float(sys.argv[2])
     b = float(sys.argv[3])
+
+    result = 0
+
+    if(op == '+'):
+        result = add(a, b)
+
+    print(f"{a} {op} {b} = {result}")    
 
 if __name__ == "__main__":
     main()
